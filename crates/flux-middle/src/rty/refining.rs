3de280fcab2d846c3463deb83425b7f442ed3b3e
vrindisbacher <vrindisbacher77@gmail.com>
2026-04-22 08:05:36 -0700

    wip fixes

--- crates/flux-middle/src/rty/refining.rs
+++ crates/flux-middle/src/rty/refining.rs
@@ -747,8 +747,8 @@ where
             if let rty::BoundVariableKind::Refine(sort, _, reft_kind) = var_kind
                 && !sort.is_param()
                 && !sort.is_loc()
-                && !sort.is_unit()
-                && !sort.is_unit_adt().is_some()
+            // && !sort.is_unit()
+            // && !sort.is_unit_adt().is_some()
             {
                 let bound_reft = rty::BoundReft { var: rty::BoundVar::from(i), kind: *reft_kind };
                 Some((rty::Var::Bound(INNERMOST, bound_reft), sort.clone()))
@@ -779,11 +779,13 @@ fn make_kvar(
     self_args: Vec<(rty::Var, rty::Sort)>,
     params: Vec<(rty::Var, rty::Sort)>,
 ) -> rty::KVar {
+    let num_self_args = self_args.len();
     let (args, sorts): (Vec<rty::Var>, Vec<rty::Sort>) =
         self_args.into_iter().chain(params.into_iter()).unzip();
     //VR(TODO): I don't quite understand why this was self_args.len() originally but anyhow I think in our
     // case we always want every param to be an argument of the kvar
-    let num_self_args = args.len();
+    // let num_self_args = args.len();
+
     let arg_exprs = args.into_iter().map(|var| rty::Expr::var(var)).collect();
     kvar_map.insert(kvid.as_u32(), KvarInfo { sorts, self_args: num_self_args });
     let ret = rty::KVar { kvid, self_args: num_self_args, args: arg_exprs };
